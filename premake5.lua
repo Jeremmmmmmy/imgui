fecfec586af38d99be282cd5e03c24f26abcba88
Jeremy <jeremy.johnson.8394@gmail.com>
2023-07-31 14:46:02 -0500

    Bug in premake file

--- premake5.lua
+++ premake5.lua
@@ -7,21 +7,21 @@ project "ImGui"
 
   files
   {
-    "imconfig.h"
-    "imgui.h"
-    "imgui.cpp"
-    "imgui_draw.cpp"
-    "imgui_internal.h"
-    "imgui_widgets.cpp"
-    "imstb_rectpack.h"
+    "imconfig.h",
+    "imgui.h",
+    "imgui.cpp",
+    "imgui_draw.cpp",
+    "imgui_internal.h",
+    "imgui_widgets.cpp",
+    "imstb_rectpack.h",
     "imstb_textedit.h",
     "imstb_truetype.h",
     "imgui_demo.cpp",
-    "imgui_tables.cpp",
+    "imgui_tables.cpp"
   }
 
   filter "system:windows"
-    systemversion: "latest"
+    systemversion "latest"
     cppdialect "C++17"
     staticruntime "On"
 
